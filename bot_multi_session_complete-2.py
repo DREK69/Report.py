f8133bf850063dccbd12b0447067d6948b1730a6
DREK <userbot870@gmail.com>
2026-02-03 10:29:50 +0530

    Update bot_multi_session_complete-2.py

--- bot_multi_session_complete-2.py
+++ bot_multi_session_complete-2.py
@@ -81,7 +81,7 @@ BOT_API_ID = 25723056
 BOT_API_HASH = "cbda56fac135e92b755e1243aefe9697"
 BOT_TOKEN = "8528337956:AAGU7PX6JooceLLL7HkH_LJ27v-QaKyrZVw"
 
-ADMIN_USER_IDS = [8528337956]
+ADMIN_USER_IDS = [8101867786]
 
 REASON_MAP = {
     1: ("Spam Messages", InputReportReasonSpam, "Unsolicited bulk messages or advertisements"),
